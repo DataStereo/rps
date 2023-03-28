214e7250684051ee032318889f1e7c50493de66d
Oleh Morozov <morozov89oleg@gmail.com>
2023-03-28 22:08:06 +0300

    added game object

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,9 @@
+package application;
+
+import game.Game;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Game().run();
     }
 }
