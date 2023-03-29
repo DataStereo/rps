5f9e69efb795376861b3cacc604980b91b2c05bf
Oleh Morozov <morozov89oleg@gmail.com>
2023-03-29 08:19:11 +0300

    adding choosing objects

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,10 +2,6 @@ package application;
 
 import game.Game;
 
-// TODO Assign names and ids to subclasses
-// TODO Pick on subclass at random and create object from it in run ()
-// TODO Show that objects package really is a subclass
-
 public class Main {
     public static void main(String[] args) {
         new Game().run();
--- src/game/Game.java
+++ src/game/Game.java
@@ -5,11 +5,18 @@ import game.objects.Paper;
 import game.objects.Rock;
 import game.objects.Scissors;
 
+import java.util.Random;
+
 public class Game {
     private GameObject[] objects = {new Rock(), new Paper(), new Scissors()};
 
     public void run(){
         System.out.println("Game running!!");
-        System.out.println(new Rock());
+
+        Random random = new Random();
+
+        GameObject obj = objects[random.nextInt(objects.length)];
+
+        System.out.println(obj);
     }
 }
