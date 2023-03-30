acb4ed63acdbb01d05bf8cf124a7e506ef329721
Oleh Morozov <morozov89oleg@gmail.com>
2023-03-30 22:25:20 +0300

    added a game loop

--- src/game/Game.java
+++ src/game/Game.java
@@ -18,20 +18,23 @@ public class Game {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
 
-        GameObject obj1 = objects[random.nextInt(objects.length)];
+        for (int i=0; i<3; i++){
+            GameObject obj1 = objects[random.nextInt(objects.length)];
 
-        System.out.println("Choose an object");
-        System.out.println("1. Rock");
-        System.out.println("2. Paper");
-        System.out.println("3. Scissors");
-        System.out.print("> ");
+            System.out.println("Choose an object");
+            System.out.println("1. Rock");
+            System.out.println("2. Paper");
+            System.out.println("3. Scissors");
+            System.out.print("> ");
 
-        int objectIndex = scanner.nextInt();
-        GameObject obj2 = objects[objectIndex -1];
+            int objectIndex = scanner.nextInt();
+            GameObject obj2 = objects[objectIndex -1];
 
-        System.out.println("You choose " + obj2);
-        System.out.println("Computer chose " + obj1);
-        System.out.println(status[obj2.compareTo(obj1) + 1]);
+            System.out.println("You choose " + obj2);
+            System.out.println("Computer chose " + obj1);
+            System.out.println(status[obj2.compareTo(obj1) + 1]);
+            System.out.println("\n");
+        }
         scanner.close();
     }
 }
